a3a280424a6ce9e4b1b63fcfde92c81008c0bf24
Italo(Hy0) <italodecarvalhocosta@gmail.com>
2026-08-07 10:24:32 -0300

    Criação dos models Cliente e Usuário

--- ProjetoCC/usuarios/models.py
+++ ProjetoCC/usuarios/models.py
@@ -1,3 +1,48 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
+
 # Create your models here.
+
+class Usuario(AbstractUser):
+
+    telefone = models.CharField(
+        max_length=20,
+        blank=True,
+        null=True
+    )
+
+# Cliente
+
+from django.db import models
+from usuarios.models import Usuario
+
+
+class Cliente(models.Model):
+
+    usuario = models.OneToOneField(
+        Usuario,
+        on_delete=models.CASCADE
+    )
+
+    data_nascimento = models.DateField()
+
+    altura = models.FloatField()
+
+    peso = models.FloatField()
+
+    sexo = models.CharField(
+        max_length=10,
+        choices=[
+            ('F', 'Feminino'),
+            ('M', 'Masculino'),
+        ]
+    )
+
+    objetivo = models.CharField(
+        max_length=100
+    )
+
+    def __str__(self):
+        return self.usuario.username
