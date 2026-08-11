ffe745e5f8728813bb967a24d1bfc0d5dd1ac0ab
Italo(Hy0) <italodecarvalhocosta@gmail.com>
2026-08-11 09:42:01 -0300

    Criação do menu protótipo

--- ProjetoCC/PlanoFit/settings.py
+++ ProjetoCC/PlanoFit/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'dieta',
     'treino',
     'monitoramento',
+    'menu',
 ]
 
 MIDDLEWARE = [
@@ -58,7 +59,7 @@ ROOT_URLCONF = 'PlanoFit.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
